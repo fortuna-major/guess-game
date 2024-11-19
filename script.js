36aa5e8311c580f2b053fa1c140308805b06e832
fortuna-major <brvmyfx@gmail.com>
2024-11-19 19:22:40 +0100

    script.js

--- script.js
+++ script.js
@@ -71,6 +71,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '😏 too high';
